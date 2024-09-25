4d5a5c838cbc467515ed086e2289eeabf5098540
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-09-25 11:32:04 +0800

    Custom build

---
+++ quick-build-cortex-a55.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+XCFLAGS="-Wno-unused-command-line-argument -mcpu=cortex-a55+crypto+ssbs -mtune=cortex-a55 -Ofast -flto=full -fno-common -fno-plt -fno-semantic-interposition -fcf-protection=none -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-ast-use-context -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fuse-ld=lld -s -Wl,-O3,--as-needed,--gc-sections,--icf=all,-z,lazy,-z,norelro,-sort-common"
+export CGO_CFLAGS="$XCFLAGS"
+export CGO_CXXFLAGS="$XCFLAGS"
+export CGO_LDFLAGS="$XCFLAGS"
+export CGO_ENABLED=0
+export GOEXPERIMENT=newinliner
+export GOARM64=v8.2,lse,crypto
+
+export CFLAGS="$XCFLAGS"
+export LDFLAGS="$XCFLAGS"
+
+pushd XrayCore
+go install golang.org/x/mobile/cmd/gomobile@latest
+# go mod tidy
+go mod download
+gomobile init
+gomobile bind -o "../app/libs/XrayCore.aar" -androidapi 26 -target "android/arm64" -gcflags=all="-B" -ldflags="-s -w -buildid=" -trimpath 
+popd
+
+./gradlew -PabiId=2 -PabiTarget=arm64-v8a assembleRelease
